7d344a898876a6b2dd88fb61b8c6a2249798cd7a
velesbh <velesbh0@gmail.com>
2025-12-24 00:43:49 +0000

    added headless support

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    open: false,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
